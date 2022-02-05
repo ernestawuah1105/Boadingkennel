0427986ba8f086322f239d0ce77d269de244ef02
ernest awuah <ernestawuah@yahoo.com>
2022-02-05 17:28:46 -0500

    intial commit

---
+++ src/main/java/com/tutoring/boardingkennel/BoardingkennelApplication.java
@@ -0,0 +1,13 @@
+package com.tutoring.boardingkennel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingkennelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingkennelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoring/boardingkennel/BoardingkennelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoring.boardingkennel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingkennelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
