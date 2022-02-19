363db60b680b2e72e182ebea6976d0eeed11b817
ernest awuah <ernestawuah@yahoo.com>
2022-02-19 17:36:16 -0500

    Homepage and navbar

--- boardingkennel-ui/src/App.js
+++ boardingkennel-ui/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './component/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App-header">
+        <Navbar bg="dark" variant="dark" expanded="true" expand="lg" fixed="top">
+            <Navbar.Brand href="/">Boarding Kennel</Navbar.Brand>
+          </Navbar>
+           <div className="componenet">
+            <Route path="/" exact component={Home}/>
+          </div>
+        
+        </div>
+      </Router>
     </div>
   );
 }
