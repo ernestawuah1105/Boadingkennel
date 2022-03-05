dae7109a227fc2a5495c33f69208caf73685d78f
ernest awuah <ernestawuah@yahoo.com>
2022-03-05 17:31:44 -0500

    created a checkin page

--- boardingkennel-ui/src/App.js
+++ boardingkennel-ui/src/App.js
@@ -5,6 +5,7 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './component/Home';
+import CheckIn from './component/CheckIn';
 
 
 function App() {
@@ -14,11 +15,15 @@ function App() {
         <div className="App-header">
         <Navbar bg="dark" variant="dark" expanded="true" expand="lg" fixed="top">
             <Navbar.Brand href="/">Boarding Kennel</Navbar.Brand>
+            <Nav>
+               <Nav.Link href="checkin">Check In</Nav.Link>
+            </Nav>
           </Navbar>
            <div className="componenet">
              <Routes>
              
             <Route path="/"  element={<Home />}/>
+            <Route path="/checkin"  element={<CheckIn />}/>
             </Routes>
             
           </div>
---
+++ boardingkennel-ui/src/component/CheckIn.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CheckIn extends Component {
+    render(){
+        return (
+           <div>
+                <p>
+          This is where you check in
+        </p>
+           </div> 
+        );
+    }
+
+}
+
+export default CheckIn;
